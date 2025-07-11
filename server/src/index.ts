55e26209a8f9c33ce15f87fd72fcc1de75a5a714
nikhil <nikhilmjgp@gmail.com>
2025-07-11 15:51:49 +0530

    courses added

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,42 +4,74 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import * as dyanmoose from "dynamoose";
-
+import * as dynamoose from "dynamoose";
+import serverless from "serverless-http";
+import seed from "./seed/seedDynamodb";
+import {
+  clerkMiddleware,
+  createClerkClient,
+  requireAuth,
+} from "@clerk/express";
+/* ROUTE IMPORTS */
 import courseRoutes from "./routes/courseRoutes";
-import {createClerkClient} from "@clerk/express";
 import userClerkRoutes from "./routes/userClerkRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
+import userCourseProgressRoutes from "./routes/userCourseProgressRoutes";
 
-dotenv.config()
-
-const isProduction = process.env.NODE_ENV ==="production";
-if(!isProduction){
-    dyanmoose.aws.ddb.local();
+/* CONFIGURATIONS */
+dotenv.config();
+const isProduction = process.env.NODE_ENV === "production";
+if (!isProduction) {
+  dynamoose.aws.ddb.local();
 }
+console.log("CLERK_SECRET_KEY:", process.env.CLERK_SECRET_KEY);
+console.log("CLERK_PUBLISHABLE_KEY:", process.env.CLERK_PUBLISHABLE_KEY)
+;
  export const clerkClient = createClerkClient({
-    secretKey: process.env.CLERK_SECRET_KEY,
- })
-const app = express();
+  
+  secretKey: process.env.CLERK_SECRET_KEY,
+  publishableKey: process.env.CLERK_PUBLISHABLE_KEY, 
+});
+console.log("clerk publishable key:", process.env.CLERK_PUBLISHABLE_KEY);
 
+const app = express();
 app.use(express.json());
 app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
-
-app.get("/",(req,res)=>{
-    res.send("Hello world");
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
-app.use("/courses",courseRoutes)
-app.use("/users/clerk",userClerkRoutes);
 
+app.use("/courses", courseRoutes);
+app.use("/users/clerk", requireAuth(), userClerkRoutes);
+app.use("/transactions", requireAuth(), transactionRoutes);
+app.use("/users/course-progress", requireAuth(), userCourseProgressRoutes);
+
+/* SERVER */
 const port = process.env.PORT || 3000;
-if(!isProduction){
-    app.listen(port,()=>{
-        console.log(`Server running on port ${port}`);
-    })
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 }
 
+// aws production environment
+const serverlessApp = serverless(app);
+export const handler = async (event: any, context: any) => {
+  if (event.action === "seed") {
+    await seed();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Data seeded successfully" }),
+    };
+  } else {
+    return serverlessApp(event, context);
+  }
+};
--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,8 +1,29 @@
 import express from "express";
-import { getCourse, listCourses } from "../controllers/courseController";
-const router = express.Router();
+import multer from "multer";
+import {
+  createCourse,
+  deleteCourse,
+  getCourse,
+  listCourses,
+  updateCourse,
+  getUploadVideoUrl,
+} from "../controllers/courseController";
+import { requireAuth } from "@clerk/express";
 
-router.get("/",listCourses)
-router.get("/:courseId",getCourse);
+const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.get("/", listCourses);
+router.post("/", requireAuth(), createCourse);
+
+router.get("/:courseId", getCourse);
+router.put("/:courseId", requireAuth(), upload.single("image"), updateCourse);
+router.delete("/:courseId", requireAuth(), deleteCourse);
+
+router.post(
+  "/:courseId/sections/:sectionId/chapters/:chapterId/get-upload-url",
+  requireAuth(),
+  getUploadVideoUrl
+);
 
 export default router;
